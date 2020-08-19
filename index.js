386d8bc9eec32fc208e44d5ec9f1b5af370e966a
alanmatw <alanmatw@gmail.com>
2020-08-19 22:32:29 +0530

    feat(index.js): configure cors and body-parser

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
  
@@ -6,6 +8,11 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Server is up!')
 })
+
+// middlewares
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()) // parse application/json
 
 // initialize server
 app.listen(process.env.port || 3000, () => {
